1f29ec9cd27e80e3ce9614f31e0ae1d1558ff317
Luca Belardinelli <belardinelliluca@gmail.com>
2025-02-06 18:23:56 +0100

    [creation routers]

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ const express = require('express')
 const app = express()
 // DEFINIAMO LA PORTA
 const port = 3000
+//importiamo il router 
+const postsRouter = require('./routers/posts')
 
+
+// DEFINIAMO USA DI UNA CARTELLA PER I FILE STATICI
+app.use(express.static('public'));
 
 // DEFINIAMO LA PRIMA ROTTA
 app.get('/', (req, res) => {
     res.send('Server del mio blog')
 })
+
+// router
+app.use("/posts", postsRouter)
 
 // AVVIAMO IL SERVER METTENDELO IN ASCOLTO SULLA PORTA INDICATA
 app.listen(port, () => {
